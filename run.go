9b29c07d26b71b5d7c378533c548bf4224e39836
Donald Linnemeyer <dlinnemeyer@gmail.com>
2017-11-26 22:19:25 -0600

    Added postgres and basic query; currently failing on scan to struct because of nils

--- run.go
+++ run.go
@@ -1,19 +1,57 @@
 package main
 
 import (
+    "fmt"
 	"net/http"
 	"log"
 	"github.com/gorilla/mux"
+    "encoding/json"
+    "database/sql"
+    "gopkg.in/doug-martin/goqu.v4"
+    _ "gopkg.in/doug-martin/goqu.v4/adapters/postgres"
+    _ "github.com/lib/pq"
 )
 
-func YourHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Gorilla!\n"))
+var db *goqu.Database
+func SetDB() {
+    pgDb, err := sql.Open("postgres", "host=psql user=admin password=admin dbname=shakespeare sslmode=disable ")
+    if err != nil {
+        panic(err.Error())
+    }
+    db = goqu.New("postgres", pgDb)
+}
+
+type Work struct{
+    Id int64 `db:"workid"`
+    Title string `db:"title"`
+    LongTitle string `db:"longtitle"`
+    Year int `db:"year"`
+    GenreType string `db:"genretype"`
+    Notes string `db:"notes"`
+    Source string `db:"source"`
+    TotalWords int64 `db:"totalwords"`
+    TotalParagraphs string `db:"totalparagraphs"`
+}
+
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+    var works []Work
+    if err := db.From("work").ScanStructs(&works); err != nil{
+        fmt.Println(err.Error())
+        return
+    }
+    works_json, err := json.Marshal(works)
+    if err != nil{
+       fmt.Println(err.Error())
+       return
+    }
+    w.Write(works_json)
 }
 
 func main() {
+    SetDB()
 	r := mux.NewRouter()
 	// Routes consist of a path and a handler function.
-	r.HandleFunc("/", YourHandler)
+	r.HandleFunc("/", RootHandler)
 
 	// Bind to a port and pass our router in
 	log.Fatal(http.ListenAndServe(":8000", r))
